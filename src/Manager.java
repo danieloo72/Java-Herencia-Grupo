e8dc863a98806caa61caf281e110f2000f8ef524
JMRS92 <julitors@gmail.com>
2026-01-13 13:49:28 +0100

    V1

---
+++ src/Manager.java
@@ -0,0 +1,21 @@
+public class Manager extends Empleado {
+    protected int personasACargo;
+
+    public Manager(String dni,String nombre, int edad, double salarioBase, int antiguedadAnios,int personasACargo){
+        super(dni, nombre, edad, salarioBase);
+        this.personasACargo = personasACargo;
+
+    }
+
+    public int getPersonasACargo() {
+        return personasACargo;
+    }
+
+    public void setPersonasACargo(int personasACargo) {
+        this.personasACargo = personasACargo;
+    }
+
+    public double calcularSalario() {
+        return super.calcularSalario();
+    }
+}
