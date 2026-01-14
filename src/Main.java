65c1aeed9d523a22fde428aab7a7c750910502ad
danieloo72 <daniel07nieto@gmail.com>
2026-01-14 08:52:12 +0100

    cambio de datos

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         Empleado empleado = new Empleado("445","Jose", 23,1000.0,2);
-        Desarrollador desarrollador = new Desarrollador("445","Jose", 40,1000.0,15,"Java");
-        Manager manager = new Manager("445","Jose", 23,1000.0,23, 8);
+        Desarrollador desarrollador = new Desarrollador("646","Jaime", 40,20000.0,20,"Java");
+        Manager manager = new Manager("787","Julio", 23,5000.0,5, 8);
 
         empleado.presentarse();
         empleado.calcularSalario();
